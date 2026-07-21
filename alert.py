79745739d341a6c0a83ac2c9cb3d3c4fb26d75c5
JonahTomaw <jonah.tomaw@gmail.com>
2026-07-21 15:04:17 +0000

    fixed: the format of the database and the inputs and outputs

--- alert.py
+++ alert.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 import sqlite3 
 
-while True:
-    #curdate = datetime.strftime('%m/%d/%Y')
-    #curtime = datetime.strftime('%I/%M')
-    curdate = '00/00/00'
-    curtime = '00/00'
-    con = sqlite3.connect('events.db')
-    cur = con.cursor()
-    cur.execute('''SELECT title
-    FROM events
-    WHERE start_date = ?
-    AND start_time >= ?
-    AND end_time <= ?''', (curdate, curtime, curtime))
-    fetch = cur.fetchall
-    print(fetch)
-    cur.execute('DELETE FROM events WHERE end_time = ?;', (curtime))
+curtime = datetime.now().isoformat()
+con = sqlite3.connect('events.db')
+cur = con.cursor()
+
+cur.execute("""
+            SELECT title
+            FROM events
+            WHERE start_time <= ?
+            AND end_time >- ? """, (curtime, curtime))
+
+event = cur.fetchall()
+print(event)
+
+cur.execute("DELETE FROM events WHERE end_time <= ?", (curtime,))
+
+con.commit()
---
+++ create_event.py
@@ -0,0 +1,28 @@
+import sqlite3
+from datetime import datetime  
+import time
+
+while True:
+    plan = input("do you want to create a new event yes or no:")
+    if plan.lower() == "yes":
+        con = sqlite3.connect('events.db')
+        cur = con.cursor()
+
+        title = input("Title:")
+
+        start = input("what time do you want it to start? Format in 'MM/DD/YY/HH/MM:")
+        dt1 = datetime.strptime(start, "%m/%d/%y/%H/%M")
+
+        end = input("Do you want an end time? Format in 'MM/DD/YY/HH/MM.:")
+        dt2 = datetime.strptime(end, "%m/%d/%y/%H/%M")
+
+        cur.execute(f"""INSERT INTO events 
+                    (start_time, end_time, title)
+                    VALUES (?, ?, ?);
+                    """, (dt1.isoformat(), dt2.isoformat(), title))
+        con.commit()
+        con.close()
+        
+        time.sleep(1)
+    if plan.lower() == "no":
+        exit()
